45bd69e7fd10b87e7d888022c23268516ef8e13d
Sushant S Durgekar <sushantdurgekar7@gmail.com>
2025-01-23 13:18:09 +0530

    Day 2

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,33 @@
-len("Hello")
+print(len("Hello"))
+
+# error about data type int
+# print(len(1234))
+
+"Hello" # this is string data type
+
+# Subscripting
+print("Hello"[0]) #H
+print("Hello"[4]) #o
+print("Hello"[-1]) #o
+
+# String
+
+print("123"+"456") # Concatenation
+
+# Integers = Whole number
+print(123+456)
+
+# Large Integers
+print(123_456_789)
+
+# Float = Floating Point Number
+print(3.14159)
+
+# Boolean
+print(True,0)
+print(False,1)
+
+
+
+
+
