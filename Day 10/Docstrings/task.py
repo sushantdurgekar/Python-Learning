c37eebff2eaded2eb448bf937e3424db54b66e66
Sushant S Durgekar <sushantdurgekar7@gmail.com>
2025-02-03 21:26:22 +0530

    Day 10 Function with return output and calculator project

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -1,10 +1,12 @@
 def format_name(f_name, l_name):
+    """ This Function takes first and last name as input and return it back in title case."""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
 
 
-formatted_name = format_name("AnGeLa", "YU")
+formatted_name = format_name(input("What is your First name: "),
+                             input("What is your Last name: "))
 
 length = len(formatted_name)
 
--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -1,7 +1,13 @@
 def format_name(f_name, l_name):
+    if f_name=="" and l_name=="":
+        return "You have provided an invalid input."
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
 
 
-print(format_name("AnGEla", "YU"))
+print(format_name(input("What is you first name: "), input("What "
+                                                           "is "
+                                                           "your "
+                                                           "last "
+                                                           "name: ")))
